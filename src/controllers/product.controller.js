4d89055e2b2f5f5f82ce94df2b5dfeb5ba1b342d
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-04-25 02:41:36 +0500

    Add smart XLSX image preprocessing with sharp

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,7 @@
+const store = require('../data/store.js');
+
+exports.getProducts = (req, res) => {
+  const { search = '' } = req.query;
+  const products = store.listProducts(search, { activeOnly: true });
+  res.json({ products });
+};
