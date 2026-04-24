ab2b033d319ab84af1581dfcc530316a24d602c1
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-04-24 18:45:55 +0500

    Delete src directory

--- src/routes/home.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const homeController = require('../controllers/home.controller.js');
-
-router.get('/home', homeController.getHome);
-
-module.exports = router;
