5bef7b528256f6363f3f22a27ab0ca15da7e7539
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-04-25 00:08:14 +0500

    Fix DALION XLSX image-row mapping and robust stock parsing

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,27 +1,19 @@
+const store = require('../data/store.js');
+
 exports.getHome = (req, res) => {
+  const mainCategories = store.getCategories({ activeOnly: true });
+  const activeProducts = store.listProducts('', { activeOnly: true });
+
   res.json({
-    banners: [
-      {
-        id: 'banner_1',
-        title: 'Tez yetkazib berish',
-        subtitle: '30 daqiqa ichida buyurtma bering'
-      }
-    ],
-    main_categories: [
-      { id: 'cat_1', name: 'Ichimliklar' },
-      { id: 'cat_2', name: 'Shirinliklar' },
-      { id: 'cat_3', name: 'Sut mahsulotlari' },
-      { id: 'cat_4', name: 'Boshqa' }
-    ],
-    popular_products: [
-      { id: '1', name: 'Coca Cola 1L', price: 14000, image: '', category_id: 'cat_1' },
-      { id: '2', name: 'Pepsi 1L', price: 13000, image: '', category_id: 'cat_1' },
-      { id: '3', name: 'Chocolate Bar', price: 9000, image: '', category_id: 'cat_2' }
-    ],
+    banners: store.getBanners({ activeOnly: true }),
+    promotions: store.getPromotions({ activeOnly: true }),
+    home_settings: store.getHomeSettings(),
+    main_categories: mainCategories,
+    popular_products: activeProducts.slice(0, 3),
     delivery_info: {
-      base_km: 3,
-      base_price: 12000,
-      price_per_km: 2500
+      location: 'Yunusobod, Toshkent',
+      time: '30 daqiqa',
+      price: 12000
     }
   });
 };
