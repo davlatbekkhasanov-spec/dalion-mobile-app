c00d96cf49534e3e193261a43c8225f0a830deaf
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-04-25 17:32:44 +0500

    Add admin category image upload, i18n switch, and file store persistence

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const router = express.Router();
-
-const homeController = require('../controllers/home.controller.js');
-
-router.get('/home', homeController.getHome);
-
-module.exports = router;
+module.exports = require('./api.routes.js');
